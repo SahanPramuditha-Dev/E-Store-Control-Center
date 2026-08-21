109bc20b878be3ff47d0108709b995b949e5ac63
Sahan Pramuditha <sahan.dev.tech@gmail.com>
2026-08-21 20:16:55 +0530

    Remove wrangler.toml and worker.js for standard Cloudflare Pages build

--- worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (env.ASSETS) {
-      return env.ASSETS.fetch(request);
-    }
-    return new Response("E-Store Control Center Live", { status: 200 });
-  },
-};
